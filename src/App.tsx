0e17365359b35e42b0311e25dc92635feec47a0d
Anh <qanh171002@gmail.com>
2025-07-27 18:16:13 +0700

    feat(dashboard): UI layout and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
@@ -6,23 +6,28 @@ import "react-toastify/dist/ReactToastify.css";
 import Signin from "./pages/Signin";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import Dashboard from "./pages/Dashboard";
+import { ThemeProvider } from "./components/theme-provider";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/dashboard" element={<h1>Home Page</h1>} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Signin />} />
-          </Routes>
-        </BrowserRouter>
-        <ToastContainer />
-      </Provider>
-    </QueryClientProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<Navigate to="/dashboard" />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Signin />} />
+            </Routes>
+          </BrowserRouter>
+          <ToastContainer />
+        </Provider>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
 
