7b18ad8ee7ab6ee236fa1544e94d64dd6ca29839
Anh <qanh171002@gmail.com>
2025-07-27 17:56:01 +0700

    chore: add Redux Provider to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,24 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Signin from "./pages/Signin";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/dashboard" element={<h1>Home Page</h1>} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Signin />} />
-        </Routes>
-      </BrowserRouter>
-      <ToastContainer />
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/dashboard" element={<h1>Home Page</h1>} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Signin />} />
+          </Routes>
+        </BrowserRouter>
+        <ToastContainer />
+      </Provider>
     </QueryClientProvider>
   );
 }
